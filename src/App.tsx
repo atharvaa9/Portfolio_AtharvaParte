f11ed8507ef6db8c8fb7b08ed602d89249f0c1a8
atharvaa9 <APARTE@STEVENS.EDU>
2024-09-25 01:14:26 -0400

    Hero and 3D components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import {
-  Navbar,
+  Navbar, Hero
 } from "./components";
 // App
 const App = () => {
@@ -11,10 +11,11 @@ const App = () => {
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar hide={hide} />
+          <Hero/>
         </div>
       </div>
     </BrowserRouter>
-  );
+  )
 };
 
 export default App;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,10 @@
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
+import { Hero } from "./Hero"
 
 
 export {
     Navbar,
-    Footer
+    Footer,
+    Hero,
 };
