0b7b866cf822b2fc5154a5adee655a60cb6e388c
atharvaa9 <APARTE@STEVENS.EDU>
2024-09-25 00:25:15 -0400

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
-
+import {
+  Navbar,
+} from "./components";
 // App
 const App = () => {
-
+  const [hide, setHide] = useState(true);
   return (
     <BrowserRouter>
-      <div>
-        Hi there
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar hide={hide} />
+        </div>
       </div>
     </BrowserRouter>
   );
